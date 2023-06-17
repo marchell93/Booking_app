2d5ad85691dbfb0fd285cc40810a7ba786d4accf
mpetrovich <mark.petrovich@ramax.ru>
2023-06-17 15:02:45 +0300

    [upd] Использовать модели для эндпоинта get_hotels

--- app/main.py
+++ app/main.py
@@ -30,10 +30,13 @@ class HotelSearchArgs:
 
 @app.get('/hotels')
 def get_hotels(
-        location: str,
-        date_from: date,
-        date_to: date,
-        stars: Optional[int] = Query(None, ge=1, le=5),
-        has_spa: Optional[bool] = None,
-):
-    return date_from, date_to
+    search_args: HotelSearchArgs = Depends(),
+) -> list[SHotel]:
+    hotels = [
+        SHotel(
+            address='ул. Гагарина, 1, Алтай',
+            name='Super Hotel',
+            stars=5,
+        )
+    ]
+    return hotels

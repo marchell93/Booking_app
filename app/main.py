e57a689fba322090f5653c7163f8945bb46ce5c9
mpetrovich <mark.petrovich@ramax.ru>
2023-06-17 13:37:03 +0300

    [upd] Добавить параметры запроса в эндпоинт get_hotels

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional
+from datetime import date
 
 app = FastAPI()
 
 
 @app.get('/hotels')
-def get_hotels():
-    return "Отель Тиздар 5 звезд"
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+        has_spa: Optional[bool] = None,
+):
+    return date_from, date_to
